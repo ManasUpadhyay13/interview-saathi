be40ae828025a599b8e87f60aaa55b3107bd8a68
ManasUpadhyay13 <manasupadhyay1318@gmail.com>
2024-09-02 23:19:49 +0530

    feat: added shadcn and clerk auth

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashbaordLayout = ({ children }) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default DashbaordLayout
