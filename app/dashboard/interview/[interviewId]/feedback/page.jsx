5eed5acda7febc5d1e42f6cbbaffa01a8ab4c1b6
ManasUpadhyay13 <manasupadhyay1318@gmail.com>
2024-09-07 03:14:22 +0530

    working on the feedback page

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+"use client"
 
-const Feedback = () => {
+import { eq } from 'drizzle-orm'
+import React, { useEffect } from 'react'
+import { db } from 'utils/db'
+import { UserAnswer } from 'utils/schema'
+
+const Feedback = ({ params }) => {
+
+    const getFeedback = async () => {
+        const result = await db.select().from(UserAnswer).where(eq(UserAnswer.mockIdRef, params.intervewId)).orderBy(UserAnswer.id)
+
+        console.log(result);
+
+    }
+
+
+    useEffect(() => {
+        getFeedback()
+    }, [])
+
     return (
-        <div>Feedback</div>
+        <div className='p-10'>
+            <h2 className='text-2xl font-bold text-green-500'>Congratulation!</h2>
+            <h2 className='font-bold text-2xl'>Here is your interview feedback</h2>
+
+            <h2 className='text-primary text-lg my-3'>Your overall interview rating: <strong>7/10</strong> </h2>
+
+            <h2 className='text-sm text-gray-500'>Find below interview question with correct answer, Your answer and feedback for imporvement.</h2>
+        </div>
     )
 }
 
